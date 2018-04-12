7cfea5c80cf00adefd3677ea406edbb59fd80407
JekaLion <djoni-01@mail.ru>
2018-04-12 18:09:11 +0600

    init

---
+++ EFLesson/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EFLesson
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("EFLessonConnectionString")
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ EFLesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataContext context = new DataContext())
+            {
+                User user = new User
+                {
+                    Id = 1,
+                    Login = "Admin",
+                    Password = "112"
+                };
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+        }
+    }
+}
